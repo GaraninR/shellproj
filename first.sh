00f031fba6cfd376194a98b1eb076422e0145d19
garanin <garanin@rikaol.by>
2021-03-14 11:39:24 +0300

    Shell Array has been added

--- first.sh
+++ first.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
-echo "Hello from Linux Shell"
+# echo "Hello from Linux Shell"
 
-var1=3
-var2=5
+# var1=3
+# var2=5
 
-result="Result: $var1+$var2"
+# result="Result: $var1+$var2"
 
-echo $result
+# echo $result
 
-if [ $var1 -eq 3 ];
-    then echo 'Equal';
-fi
+# if [ $var1 -eq 3 ];
+#     then echo 'Equal';
+# fi
 
-echo $1
-echo $2
+# echo $1
+# echo $2
 
-if [ $1 = "param1" ]; then echo 'Equal'; fi
+# if [ $1 = "param1" ]; then echo 'Equal'; fi
 
-echo $0
-echo $#
+# echo $0
+# echo $#
 
-echo $ENV
+# echo $ENV
 
 shell_array=(1 2 3 4 5 6 7)
-#
-#
-# comment
+
+shell_array[0]="string1"
+shell_array[1]="string2"
+
+for i in 0 1
+do
+    echo "Elem value: ${shell_array[$i]}"
+done
